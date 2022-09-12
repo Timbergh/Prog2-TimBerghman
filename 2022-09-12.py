2dbba37b35dde9245c5dfcd92e6e73811262813b
Tim Berghman <tim.berghman@gmail.com>
2022-09-12 14:32:15 +0200

    Klar

---
+++ 2022-09-12.py
@@ -0,0 +1,15 @@
+
+
+class Elev:
+    def __init__(self, namn, ålder, godkänd):
+        self.namn = namn
+        self.ålder = ålder
+        self.godkänd = godkänd
+    
+    glad = False
+
+elev1 = Elev("Tim", 17, True)
+if elev1.godkänd:
+    elev1.glad = True
+
+print(elev1.glad)
