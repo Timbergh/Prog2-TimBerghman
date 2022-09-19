36250ea48b726dbf5a6b4b540b00a2459f8078dd
Tim Berghman <tim.berghman@gmail.com>
2022-09-19 14:23:37 +0200

    Klar

---
+++ 2022-09-19.py
@@ -0,0 +1,19 @@
+
+# Uppgift 1
+
+# def addera(*nummer):
+#     summa = sum(nummer)
+#     print(summa)
+
+# addera(5, 5, 3, 9)
+
+
+#Uppgift 2
+
+# def food(s, vegan=False):
+#     if (vegan):
+#         print("soja" + s)
+#     else:
+#         print(s)
+
+# food("mjölk", True)
